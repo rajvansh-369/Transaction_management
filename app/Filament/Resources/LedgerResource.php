e97aa5f4bd002d128a872ee01619bcc932b18293
Rajvansh <106300079+steve-369@users.noreply.github.com>
2023-12-14 20:48:42 +0530

    Product | Customer added

---
+++ app/Filament/Resources/LedgerResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LedgerResource\Pages;
+use App\Filament\Resources\LedgerResource\RelationManagers;
+use App\Models\Ledger;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LedgerResource extends Resource
+{
+    protected static ?string $model = Ledger::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('customer_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('total_amount')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('total_credit')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('total_due')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('customer_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_credit')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_due')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLedgers::route('/'),
+            'create' => Pages\CreateLedger::route('/create'),
+            'edit' => Pages\EditLedger::route('/{record}/edit'),
+        ];
+    }
+}
