0ef248ad744cea2c3d5bc5312922c1400cdfce93
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-02-12 17:24:54 +0530

    print on edit added

--- app/Filament/Resources/LedgerResource/Pages/EditLedger.php
+++ app/Filament/Resources/LedgerResource/Pages/EditLedger.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\LedgerResource\Pages;
 use App\Filament\Resources\LedgerResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Request;
 
 class EditLedger extends EditRecord
 {
@@ -15,9 +16,18 @@ class EditLedger extends EditRecord
         return [
             Actions\DeleteAction::make(),
             Actions\CreateAction::make()
+            ->color("primary")
+            ->label("Print")
+            ->url(function($record){
+              $url =  Request::url();
+                $urlArray = explode("/", $url);
+                return  route('pdf', $urlArray[5]) ;
+            }),
+            Actions\CreateAction::make()
             ->color("success")
             ->label("Create New")
             ->url(env('APP_URL').'admin/ledgers/create'),
+
         ];
     }
 
