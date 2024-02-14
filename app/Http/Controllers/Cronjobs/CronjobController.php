5b7e506dcd6e92a84b97b0645b3bb3bf20106fd7
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-02-15 00:00:28 +0530

    sms api fixed

--- app/Http/Controllers/Cronjobs/CronjobController.php
+++ app/Http/Controllers/Cronjobs/CronjobController.php
@@ -16,28 +16,31 @@ class CronjobController extends Controller
 
         foreach ($allLedgers as $ledger) {
             $totalDue = $ledger['total_due'];
-            $createdAt = $ledger['invoice_date'];
+            $createdAt = Carbon::parse($ledger['invoice_date']);
             $currentDate = now();
 
             $differenceInDays = $createdAt->diffInDays($currentDate);
             if ($totalDue > 0) {
                 if($differenceInDays == 12 && $ledger->interest_amount == 0){
                     $daysAdd = 15;
-                    $date = $ledger['invoice_date'];
+                    $date =  Carbon::parse($ledger['invoice_date']);
                     $date = $date->addDays($daysAdd)->format("d-M-Y");
 
+
                     $msgTemp = "Dear " . $ledger->customer->name . "
-This is a reminder about your upcoming payment due on ".$date." Amount : " . number_format($totalDue , 2 , '.' , ',') . " Please ensure that your payment is processed by due date to avoid any 18% late fees.
+This is a reminder about your upcoming payment due on ".$createdAt->format("d-M-Y")." Amount : " . number_format($totalDue , 2 , '.' , ',') . " Please ensure that your payment is processed by due date to avoid any 18% late fees.
                     Text2";
                     // Print request parameters
-
+                    // dd($ledger , $msgTemp);
                     $response =  Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296746&authentic-key=383473616e646565706c64683130301705659905&number='. $ledger->customer->phone.'&message= '.$msgTemp.'');
+
+
                 }elseif ($differenceInDays == 15) {
 
                     $interestAmount = $totalDue * 0.18;
                     $ledger->interest_amount = $interestAmount;
                     $msgTemp = "Dear " . $ledger->customer->name . "
-This is a reminder about your upcoming payment overdue from " . $createdAt . " %26 Amount : " . number_format($totalDue+$interestAmount , 2 , '.' , ',') . " to till date. Please complete your overdue payment with 18% late fees charges.
+This is a reminder about your upcoming payment overdue from " . $createdAt->format("d-M-Y") . " %26 Amount : " . number_format($totalDue+$interestAmount , 2 , '.' , ',') . " to till date. Please complete your overdue payment with 18% late fees charges.
                     Text2";
                     Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296747&authentic-key=383473616e646565706c64683130301705659905&number='. $ledger->customer->phone.'&message='.$msgTemp.'');
                     $ledger->save();
@@ -48,7 +51,7 @@ This is a reminder about your upcoming payment overdue from " . $createdAt . " %
                     $ledger->interest_amount = $interestAmount;
 
                     $msgTemp = "Dear " . $ledger->customer->name . "
-This is a reminder about your upcoming payment overdue from " . $createdAt . " %26 Amount : " . number_format($totalDue+$interestAmount , 2 , '.' , ',') . "
+This is a reminder about your upcoming payment overdue from " . $createdAt->format("d-M-Y") . " %26 Amount : " . number_format($totalDue+$interestAmount , 2 , '.' , ',') . "
 to till date. Please complete your overdue payment with 18% late fees charges.
 Text2";
                     Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296747&authentic-key=383473616e646565706c64683130301705659905&number='. $ledger->customer->phone.'&message='.$msgTemp.'');
