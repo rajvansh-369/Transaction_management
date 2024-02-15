3884513e83e63d6dd6ad94641668c74164eea6cb
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-02-15 23:31:45 +0530

    schedule upddate

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +15,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function () {
+            // Make a request to the add-tax route
+            $response = Http::get(route('ledger.tax'));
+            // return route('ledger.tax')
+            // Log the response (optional)
+            \Illuminate\Support\Facades\Log::info($response);
+        })->everyMinute();
     }
 
     /**
--- app/Http/Controllers/Cronjobs/CronjobController.php
+++ app/Http/Controllers/Cronjobs/CronjobController.php
@@ -14,55 +14,77 @@ class CronjobController extends Controller
     {
         $allLedgers = Ledger::get();
 
-        foreach ($allLedgers as $ledger) {
-            $totalDue = $ledger['total_due'];
-            $createdAt = Carbon::parse($ledger['invoice_date']);
-            $currentDate = now();
+        return $allLedgers;
 
-            $differenceInDays = $createdAt->diffInDays($currentDate);
-            if ($totalDue > 0) {
-                if($differenceInDays == 12 && $ledger->interest_amount == 0){
-                    $daysAdd = 15;
-                    $date =  Carbon::parse($ledger['invoice_date']);
-                    $date = $date->addDays($daysAdd)->format("d-M-Y");
+//         foreach ($allLedgers as $ledger) {
+//             $totalDue = $ledger['total_due'];
+//             $createdAt = Carbon::parse($ledger['invoice_date']);
+//             $currentDate = now();
 
-
-                    $msgTemp = "Dear " . $ledger->customer->name . "
-This is a reminder about your upcoming payment due on ".$createdAt->format("d-M-Y")." Amount : " . number_format($totalDue , 2 , '.' , ',') . " Please ensure that your payment is processed by due date to avoid any 18% late fees.
-                    Text2";
-                    // Print request parameters
-                    // dd($ledger , $msgTemp);
-                    $response =  Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296746&authentic-key=383473616e646565706c64683130301705659905&number='. $ledger->customer->phone.'&message= '.$msgTemp.'');
+//             $differenceInDays = $createdAt->diffInDays($currentDate);
+//             if ($totalDue > 0) {
+//                 if ($differenceInDays == 12 && $ledger->interest_amount == 0) {
+//                     $daysAdd = 15;
+//                     $date =  Carbon::parse($ledger['invoice_date']);
+//                     $date = $date->addDays($daysAdd)->format("d-M-Y");
 
 
-                }elseif ($differenceInDays == 15) {
+//                     $msgTemp = "Dear " . $ledger->customer->name . "
+// This is a reminder about your upcoming payment due on " . $createdAt->format("d-M-Y") . " Amount : " . number_format($totalDue, 2, '.', ',') . " Please ensure that your payment is processed by due date to avoid any 18% late fees.
+//                     Text2";
+//                     // Print request parameters
 
-                    $interestAmount = $totalDue * 0.18;
-                    $ledger->interest_amount = $interestAmount;
-                    $msgTemp = "Dear " . $ledger->customer->name . "
-This is a reminder about your upcoming payment overdue from " . $createdAt->format("d-M-Y") . " %26 Amount : " . number_format($totalDue+$interestAmount , 2 , '.' , ',') . " to till date. Please complete your overdue payment with 18% late fees charges.
-                    Text2";
-                    Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296747&authentic-key=383473616e646565706c64683130301705659905&number='. $ledger->customer->phone.'&message='.$msgTemp.'');
-                    $ledger->save();
-                } elseif ($differenceInDays > 15 && $differenceInDays % 3 == 0) {
-                    $additionalTax = ($totalDue + $ledger->interest_amount) * 0.18;
+//                     // dd($ledger , $msgTemp);
+//                     $response =  Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296746&authentic-key=383473616e646565706c64683130301705659905&number=' . $ledger->customer->phone . '&message= ' . $msgTemp . '');
 
-                    $interestAmount = $ledger->interest_amount + $additionalTax;
-                    $ledger->interest_amount = $interestAmount;
+//                     $responseArray = [
 
-                    $msgTemp = "Dear " . $ledger->customer->name . "
-This is a reminder about your upcoming payment overdue from " . $createdAt->format("d-M-Y") . " %26 Amount : " . number_format($totalDue+$interestAmount , 2 , '.' , ',') . "
-to till date. Please complete your overdue payment with 18% late fees charges.
-Text2";
-                    Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296747&authentic-key=383473616e646565706c64683130301705659905&number='. $ledger->customer->phone.'&message='.$msgTemp.'');
+//                         "sms_response" => json_decode($response, true),
+//                         'sms' => $msgTemp
+//                     ];
+//                     return $responseArray;
+//                 } elseif ($differenceInDays == 15) {
 
-                    $ledger->save();
-                } else {
-                    continue;
-                }
-            } else {
-                continue;
-            }
-        }
+//                     $interestAmount = $totalDue * 0.18;
+//                     $ledger->interest_amount = $interestAmount;
+//                     $msgTemp = "Dear " . $ledger->customer->name . "
+// This is a reminder about your upcoming payment overdue from " . $createdAt->format("d-M-Y") . " %26 Amount : " . number_format($totalDue + $interestAmount, 2, '.', ',') . " to till date. Please complete your overdue payment with 18% late fees charges.
+//                     Text2";
+//                     $response = Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296747&authentic-key=383473616e646565706c64683130301705659905&number=' . $ledger->customer->phone . '&message=' . $msgTemp . '');
+//                     $ledger->save();
+
+//                     $responseArray = [
+
+//                         "sms_response" => json_decode($response, true),
+//                         'sms' => $msgTemp
+//                     ];
+//                     return $responseArray;
+//                 } elseif ($differenceInDays > 15 && $differenceInDays % 3 == 0) {
+//                     $additionalTax = ($totalDue + $ledger->interest_amount) * 0.18;
+
+//                     $interestAmount = $ledger->interest_amount + $additionalTax;
+//                     $ledger->interest_amount = $interestAmount;
+
+//                     $msgTemp = "Dear " . $ledger->customer->name . "
+// This is a reminder about your upcoming payment overdue from " . $createdAt->format("d-M-Y") . " %26 Amount : " . number_format($totalDue + $interestAmount, 2, '.', ',') . "
+// to till date. Please complete your overdue payment with 18% late fees charges.
+// Text2";
+//                     $response =  Http::get('http://sms0005.smsspeed.net/http-tokenkeyapi.php?senderid=TEXTTO&route=1&templateid=1607100000000296747&authentic-key=383473616e646565706c64683130301705659905&number=' . $ledger->customer->phone . '&message=' . $msgTemp . '');
+
+//                     $ledger->save();
+
+//                     $responseArray = [
+
+//                         "sms_response" => json_decode($response, true),
+//                         'sms' => $msgTemp
+//                     ];
+//                     return $responseArray;
+//                 } else {
+//                     continue;
+//                 }
+//             } else {
+//                 continue;
+//             }
+//         }
     }
 }
