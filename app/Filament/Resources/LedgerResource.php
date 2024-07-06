901a0d65a0f0f1e332df032b2abd9c811a58c1ab
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-07-06 17:36:35 +0530

    bill no updatedd to 1- 60

--- app/Filament/Resources/LedgerResource.php
+++ app/Filament/Resources/LedgerResource.php
@@ -59,16 +59,16 @@ class LedgerResource extends Resource
                         $ledger = Ledger::orderBy('created_at', 'desc')->first();
 
 
-                        return   $ledger ?  $ledger->bill_no + 1 : 1;
-                        // if($ledger->bill_no < 60 && $ledger->bill_no >= 1) {
+                        // return   $ledger ?  $ledger->bill_no + 1 : 1;
+                        if($ledger->bill_no < 60 && $ledger->bill_no >= 1) {
 
-                        //     return $ledger->bill_no + 1;
+                            return $ledger->bill_no + 1;
 
-                        // }elseif($ledger->bill_no  == 60){
+                        }elseif($ledger->bill_no  == 60){
 
-                        //     return   1;
+                            return   1;
 
-                        // }
+                        }
                     })
                     ->required()
                     ->readonly(),
